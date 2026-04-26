ccc8d87d7566eae890f2749bc986f8bd76b135e6
abiasa <rnabiasa@gmail.com>
2026-04-26 13:39:33 +0700

    Update: menu link

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -13,7 +13,7 @@ import {
 const navLinks = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
-  { label: "Menu", href: "/#menu" },
+  { label: "Menu", href: "/menu" },
   { label: "Gallery", href: "/gallery" },
   { label: "Contact", href: "/contact" },
 ];
